3a481069419f1bf02acfcd579e3972d583210bd7
Balasubm <48176017+Balasubm@users.noreply.github.com>
2021-12-10 18:21:41 +0530

    Update CreateContact.java

--- week2/day2.assignments/CreateContact.java
+++ week2/day2.assignments/CreateContact.java
@@ -1,54 +1,40 @@
-package week2.day2.Assignments;
+package week4.day2;
 
-public class CreateContact {
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Leaftaps {
 
 	public static void main(String[] args) {
-		
-		/*
-		 * //Pseudo Code
-		 * 
-		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
-		 * 
-		 * 2. Enter UserName and Password Using Id Locator
-		 * 
-		 * 3. Click on Login Button using Class Locator
-		 * 
-		 * 4. Click on CRM/SFA Link
-		 * 
-		 * 5. Click on contacts Button
-		 * 
-		 * 6. Click on Create Contact
-		 *  
-		 * 7. Enter FirstName Field Using id Locator
-		 * 
-		 * 8. Enter LastName Field Using id Locator
-		 * 
-		 * 9. Enter FirstName(Local) Field Using id Locator
-		 * 
-		 * 10. Enter LastName(Local) Field Using id Locator
-		 * 
-		 * 11. Enter Department Field Using any Locator of Your Choice
-		 * 
-		 * 12. Enter Description Field Using any Locator of your choice 
-		 * 
-		 * 13. Enter your email in the E-mail address Field using the locator of your choice
-		 * 
-		 * 14. Select State/Province as NewYork Using Visible Text
-		 * 
-		 * 15. Click on Create Contact
-		 * 
-		 * 16. Click on edit button 
-		 * 
-		 * 17. Clear the Description Field using .clear
-		 * 
-		 * 18. Fill ImportantNote Field with Any text
-		 * 
-		 * 19. Click on update button using Xpath locator
-		 * 
-		 * 20. Get the Title of Resulting Page.
-
-         */
-		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Create Contact")).click();
+		driver.findElement(By.id("firstNameField")).sendKeys("Bala");
+		driver.findElement(By.id("lastNameField")).sendKeys("Mohan");
+		driver.findElement(By.id("createContactForm_description")).sendKeys("This is good contact");
+		driver.findElement(By.className("smallSubmit")).click();
+		driver.findElement(By.xpath("//a[text()= 'Edit']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("updateContactForm_description")).clear();
+		driver.findElement(By.id("updateContactForm_importantNote")).sendKeys("this is important");
+		driver.findElement(By.xpath("//input[@class='smallSubmit']")).click();
+		String title2 = driver.getTitle();
+		System.out.println(title2);
 	}
 
 }
